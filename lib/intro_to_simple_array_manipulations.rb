860410bb329389fc49df00fa5a6088ed6e815a3b
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-08 19:46:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,5 @@
-def using_push #(colors_in_the_rainbow, next_color)
-    colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-    next_color = "violet"
-    updated_array = colors_in_the_rainbow.push(next_color)
+def using_push(colors_in_the_rainbow, next_color)
+    
 
 end  
 
