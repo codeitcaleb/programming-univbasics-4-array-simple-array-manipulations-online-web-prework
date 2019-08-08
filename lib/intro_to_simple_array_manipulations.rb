8dc7eb007740b302ac21293fae3872d469d8e13d
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-08 21:16:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,7 +42,7 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete_at(array, integer)
-  array.delete_at(integer)
+def using_delete_at(array, Integer)
+  array.delete_at(Integer)
 end
 
