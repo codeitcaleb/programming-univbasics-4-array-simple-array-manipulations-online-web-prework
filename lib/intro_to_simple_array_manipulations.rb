706f55249c28d984120fb04dc5bc15deeefabcca
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-08 17:55:05 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,52 @@
+def using_push
+  
+end  
+
+def using_unshift
+  
+end  
+
+def using_pop
+  
+end  
+
+def using_push
+  
+end  
+
+def pop_with_args
+  
+end  
+
+def using_shift 
+  
+end 
+
+def shift_with_args
+  
+end
+
+def using_concat
+  
+end
+
+def using_insert
+  
+end
+
+def using_uniq 
+  
+end
+
+def using_flatten
+  
+end  
+
+def using_delete
+  
+end
+
+def using_delete_at
+  
+end
+
